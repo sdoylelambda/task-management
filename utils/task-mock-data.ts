e6996edfed17f5f0710773cd98465c0bf886926f
metatronOO7 <42658878+metatronOO7@users.noreply.github.com>
2024-07-24 15:22:42 -0400

    Display github pages.

--- utils/task-mock-data.ts
+++ utils/task-mock-data.ts
@@ -8,7 +8,7 @@ export const sampleTaskData: TaskInterface[] = [
   { id: 103, name: "Call Friend", description: "Call a friend I haven't talked to in awhile.", catagory: "Family & Friends" },
   { id: 104, name: "Romance", description: "Take a date for a walk on the beach.", catagory: "Significant Other/Romance" },
   { id: 105, name: "Nature Walk", description: "Go somewhere I have not been before.", catagory: "Personal Growth" },
-  { id: 106, name: "Sign up CDS", description: "Use PC for easier setup.", catagory: "Money" },
+  { id: 106, name: "Sign up CDS", description: "Use a PC for easier setup.", catagory: "Money" },
   { id: 107, name: "Fruit", description: "Get some fruit today. At least a drink.", catagory: "Health" },
   { id: 108, name: "Motorcycle", description: "Go for a ride and enjoy life!", catagory: "Fun & Recreation" },
 ];
